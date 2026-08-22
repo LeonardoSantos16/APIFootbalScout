c05098f11cc95763b8c16c568cacc3d68ec7f5fb
Leonardo Santos <leodsmf@gmail.com>
2026-08-22 20:16:33 -0300

    feat: injeta a chave de assinatura JWT pelo AppHost

--- APIFootballScout.AppHost/AppHost.cs
+++ APIFootballScout.AppHost/AppHost.cs
@@ -5,6 +5,7 @@ var mongodb = mongoServer.AddDatabase("scoutdb");
 
 var cache = builder.AddRedis("cache");
 var sofaKey = builder.AddParameter("SofaScoreKey", secret: true);
+var jwtKey = builder.AddParameter("JwtSigningKey", secret: true);
 
 builder.AddProject<Projects.APIFootballScout>("apifootballscout")
     .WithReference(mongoServer)
@@ -12,6 +13,7 @@ builder.AddProject<Projects.APIFootballScout>("apifootballscout")
     .WithReference(cache)
     .WaitFor(mongodb)
     .WaitFor(cache)
+    .WithEnvironment("Jwt__Key", jwtKey)
     .WithEnvironment("SofaScore__ApiKey", sofaKey);
 
 builder.Build().Run();
