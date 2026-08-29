b2bba1fe24178b0ad22ee02b9037b0e49e3457ad
Leonardo Santos <leodsmf@gmail.com>
2026-08-28 23:51:36 -0300

    test: cobre a comparabilidade das leituras atraves da virada de temporada (R2.5)

---
+++ APIFootballScout/Domain/Acompanhamento/Specifications/LeiturasComparaveisSpecification.cs
@@ -0,0 +1,11 @@
+using APIFootballScout.Domain.Acompanhamento.ValueObject;
+using APIFootballScout.Domain.Base;
+
+namespace APIFootballScout.Domain.Acompanhamento.Specifications
+{
+    public sealed class LeiturasComparaveisSpecification : Specification<ComMudanca>
+    {
+        public override bool IsSatisfiedBy(ComMudanca mudanca) =>
+            throw new NotImplementedException();
+    }
+}
