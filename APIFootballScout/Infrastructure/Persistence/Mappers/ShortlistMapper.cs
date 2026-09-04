f48439b7e96847fc285476b075f621753984c227
Leonardo Santos <leodsmf@gmail.com>
2026-09-04 01:33:59 -0300

    test: cobre a persistencia da shortlist (F7)

---
+++ APIFootballScout/Infrastructure/Persistence/Mappers/ShortlistMapper.cs
@@ -0,0 +1,14 @@
+using APIFootballScout.Domain.ShortlistPersonalizada.Agreggate;
+using APIFootballScout.Infrastructure.Persistence.Documents;
+
+namespace APIFootballScout.Infrastructure.Persistence.Mappers
+{
+    internal static class ShortlistMapper
+    {
+        public static Shortlist MapToDomain(ShortlistDocument document)
+            => throw new NotImplementedException();
+
+        public static ShortlistDocument MapToEntity(Shortlist shortlist)
+            => throw new NotImplementedException();
+    }
+}
