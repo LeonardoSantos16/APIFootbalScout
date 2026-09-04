93fe34c91da9958a71878fe422e740aca4c9e988
Leonardo Santos <leodsmf@gmail.com>
2026-09-03 23:05:14 -0300

    feat: implementa o fluxo da repriorizacao de alvo (F7)

--- APIFootballScout/Application/ShortlistPersonalizada/RepriorizarAlvoUseCase.cs
+++ APIFootballScout/Application/ShortlistPersonalizada/RepriorizarAlvoUseCase.cs
@@ -1,4 +1,7 @@
+using APIFootballScout.Domain.Base.Exceptions;
 using APIFootballScout.Domain.Repository;
+using APIFootballScout.Domain.ShortlistPersonalizada.Agreggate;
+using APIFootballScout.Domain.ShortlistPersonalizada.ValueObject;
 
 namespace APIFootballScout.Application.ShortlistPersonalizada
 {
@@ -11,8 +14,17 @@ namespace APIFootballScout.Application.ShortlistPersonalizada
             _shortlistRepository = shortlistRepository;
         }
 
-        public Task<ShortlistResult> RepriorizarAlvo(
+        public async Task<ShortlistResult> RepriorizarAlvo(
             RepriorizarAlvoRequest request, CancellationToken cancellationToken)
-            => throw new NotImplementedException();
+        {
+            var shortlist = await _shortlistRepository.ObterPorIdAsync(request.ShortlistId, request.OlheiroId, cancellationToken) ?? throw new RecursoNaoEncontradoException("shortlist.nao_encontrada", "shortlist nao encontrada");
+            var alvo = shortlist.Alvos.FirstOrDefault(a => a.JogadorId == request.JogadorId) ?? throw new RecursoNaoEncontradoException("shortlist.alvo_nao_encontrado", "alvo nao encontrado");
+            var shortlistRestaurada = Shortlist.Restaurar(shortlist.Id, shortlist.OlheiroId, shortlist.Nome, shortlist.Limite, shortlist.Alvos);
+
+            shortlistRestaurada.AtualizarPrioridade(request.JogadorId, new Prioridade(request.Prioridade));
+
+            await _shortlistRepository.AtualizarAsync(shortlistRestaurada, cancellationToken);
+            return shortlistRestaurada.ParaResult();
+        }
     }
 }
