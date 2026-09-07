4a1937eaf479d8d9f4c53bf801d566a7928653d5
Leonardo Santos <leodsmf@gmail.com>
2026-09-07 16:52:01 -0300

    test: cobre a amostra minima de minutos (F9)

---
+++ APIFootballScout/Domain/Analise/Specifications/AmostraSuficienteSpecification.cs
@@ -0,0 +1,12 @@
+using APIFootballScout.Domain.Acompanhamento.ValueObject;
+using APIFootballScout.Domain.Analise.ValueObject;
+using APIFootballScout.Domain.Base;
+
+namespace APIFootballScout.Domain.Analise.Specifications
+{
+    public sealed class AmostraSuficienteSpecification(AmostraMinima minima) : Specification<Minutagem>
+    {
+        public override bool IsSatisfiedBy(Minutagem minutagem)
+            => throw new NotImplementedException();
+    }
+}
