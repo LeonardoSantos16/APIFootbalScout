43bbda9100acbece01cf04e305768c1ce212d2a0
Leonardo Santos <leodsmf@gmail.com>
2026-09-07 17:59:19 -0300

    feat: exige amostra que sustente a metrica por 90 (F9)

--- APIFootballScout/Domain/Analise/ValueObject/MetricaCalculada.cs
+++ APIFootballScout/Domain/Analise/ValueObject/MetricaCalculada.cs
@@ -1,6 +1,24 @@
 using APIFootballScout.Domain.Acompanhamento.ValueObject;
+using APIFootballScout.Domain.Base.Exceptions;
 
 namespace APIFootballScout.Domain.Analise.ValueObject
 {
-    public sealed record MetricaCalculada(decimal Valor, Minutagem Amostra) : MetricaPor90;
+    public sealed record MetricaCalculada : MetricaPor90
+    {
+        public decimal Valor { get; init; }
+        public Minutagem Amostra { get; init; }
+
+        public MetricaCalculada(decimal valor, Minutagem amostra)
+        {
+            if (amostra.Minutos <= 0)
+            {
+                throw new ValorInvalidoException(
+                    "metrica_por_90.amostra_nao_sustenta",
+                    "A metrica precisa declarar uma amostra de minutos positiva.");
+            }
+
+            Valor = valor;
+            Amostra = amostra;
+        }
+    }
 }
