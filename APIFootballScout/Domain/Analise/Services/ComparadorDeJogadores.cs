b3330fdee7710e61cd8658e7b971a693b974ef04
Leonardo Santos <leodsmf@gmail.com>
2026-09-08 21:16:10 -0300

    feat: implementa as regras da comparacao direta no dominio (F10)

--- APIFootballScout/Domain/Analise/Services/ComparadorDeJogadores.cs
+++ APIFootballScout/Domain/Analise/Services/ComparadorDeJogadores.cs
@@ -1,5 +1,7 @@
 using APIFootballScout.Domain.Analise.Specifications;
 using APIFootballScout.Domain.Analise.ValueObject;
+using APIFootballScout.Domain.Base.Exceptions;
+using APIFootballScout.Domain.CatalogoDeJogador;
 
 namespace APIFootballScout.Domain.Analise.Services
 {
@@ -17,6 +19,59 @@ namespace APIFootballScout.Domain.Analise.Services
         }
 
         public ResultadoDaComparacao Comparar(JogadorParaComparar um, JogadorParaComparar outro)
-            => throw new NotImplementedException();
+        {
+            if (um.Perfil.JogadorId == outro.Perfil.JogadorId)
+            {
+                throw new ValorInvalidoException(
+                    "comparacao.jogador_consigo_mesmo",
+                    "A comparacao exige dois jogadores distintos.");
+            }
+
+            if (um.Perfil.Posicao is not Posicao posicaoDeUm
+                || outro.Perfil.Posicao is not Posicao posicaoDoOutro)
+            {
+                return new ComparacaoRecusada(MotivoDaRecusaDaComparacao.PosicaoDesconhecida);
+            }
+
+            if (!_posicoesCompativeis.IsSatisfiedBy((posicaoDeUm, posicaoDoOutro)))
+            {
+                return new ComparacaoRecusada(MotivoDaRecusaDaComparacao.PosicoesIncompativeis);
+            }
+
+            if (um.Estatisticas.Recorte != outro.Estatisticas.Recorte)
+            {
+                return new ComparacaoRecusada(MotivoDaRecusaDaComparacao.RecorteDivergente);
+            }
+
+            if (!_amostraSuficiente.IsSatisfiedBy(um.Estatisticas.Minutagem)
+                || !_amostraSuficiente.IsSatisfiedBy(outro.Estatisticas.Minutagem))
+            {
+                return new ComparacaoRecusada(MotivoDaRecusaDaComparacao.AmostraInsuficiente);
+            }
+
+            var deUm = ResultadosPorTipo(um);
+            var doOutro = ResultadosPorTipo(outro);
+
+            var atributos = deUm.Keys
+                .Where(doOutro.ContainsKey)
+                .OrderBy(tipo => tipo)
+                .Select(tipo => new AtributoComparado(
+                    tipo,
+                    new Dictionary<int, ResultadoDeAtributo>
+                    {
+                        [um.Perfil.JogadorId] = deUm[tipo],
+                        [outro.Perfil.JogadorId] = doOutro[tipo]
+                    }))
+                .ToList();
+
+            return new ComparacaoRealizada(um.Estatisticas.Recorte, atributos);
+        }
+
+        private Dictionary<TipoDeAtributo, ResultadoDeAtributo> ResultadosPorTipo(JogadorParaComparar jogador)
+            => jogador.Estatisticas.Acumulaveis
+                .Select(estatistica => (estatistica.Tipo, Resultado: estatistica.PorNoventaMinutos(_amostraSuficiente)))
+                .Concat(jogador.Estatisticas.Derivados
+                    .Select(derivado => (derivado.Tipo, Resultado: derivado.Resultado())))
+                .ToDictionary(atributo => atributo.Tipo, atributo => atributo.Resultado);
     }
 }
--- APIFootballScout/Domain/Analise/Specifications/PosicoesCompativeisSpecification.cs
+++ APIFootballScout/Domain/Analise/Specifications/PosicoesCompativeisSpecification.cs
@@ -3,12 +3,25 @@ using APIFootballScout.Domain.CatalogoDeJogador;
 
 namespace APIFootballScout.Domain.Analise.Specifications
 {
-    public sealed class PosicoesCompativeisSpecification : Specification<(Posicao, Posicao)>
+    public sealed class PosicoesCompativeisSpecification : Specification<(Posicao Uma, Posicao Outra)>
     {
+        private readonly HashSet<(Posicao, Posicao)> _declaradas;
+
         public PosicoesCompativeisSpecification(IReadOnlyDictionary<Posicao, IReadOnlyCollection<Posicao>> mapa)
         {
+            _declaradas = [];
+
+            foreach (var (posicao, compativeis) in mapa)
+            {
+                foreach (var compativel in compativeis)
+                {
+                    _declaradas.Add((posicao, compativel));
+                    _declaradas.Add((compativel, posicao));
+                }
+            }
         }
 
-        public override bool IsSatisfiedBy((Posicao, Posicao) par) => throw new NotImplementedException();
+        public override bool IsSatisfiedBy((Posicao Uma, Posicao Outra) par)
+            => par.Uma == par.Outra || _declaradas.Contains(par);
     }
 }
